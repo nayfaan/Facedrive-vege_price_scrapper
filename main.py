2df7cd4445738dfb5044fefb43fbb5178c496acc
nayfaan <luknnn129@gmail.com>
2022-04-27 12:29:19 -0400

    Now functional

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import services.startDriver
 from services.startDriver import *
+import re
 
 def initiate_driver(driver, url):
     driver.get(url)
     
-'''def output_csv(ids):
-    with open('output/output_obj.txt', 'w', newline='') as csvfile:
-        output = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for row in ids:
-            output.writerow(row)
-            print(row)'''
+def output_txt(price_list):
+    CLEANR = re.compile('<.*?>')
+    
+    with open('output/output_obj.txt', 'w', newline='') as txtfile:
+        txtfile.write("{")
+        for key, value in price_list.items():
+            txtfile.write('"%s": "%s",\n' % (key, re.sub(CLEANR, '', value)))
+        txtfile.write("}")
 
 def scrap(driver):
-    items = driver.find_elements_by_css_selector(".elementor-portfolio-item")
+    items = driver.find_elements_by_css_selector(".elementor-portfolio-item > a")
     #.get_attribute("href")
     urls = []
     for item in items:
         urls.append(item.get_attribute("href"))
-        print(urls)
     
     price_list = {}
     for url in urls:
         try:
             driver.get(url)
-            price_list[url] = driver.find_element_by_css_selector(".amount")
+            price_list[url] = driver.find_element_by_css_selector(".amount").get_attribute("innerHTML")
         except Exception as e:
             print (e)
     
     print(price_list)
+    output_txt(price_list)
     
 def run():
     driver = services.startDriver.start()
