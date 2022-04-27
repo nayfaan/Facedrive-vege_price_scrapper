73811002b5004cb30c8472baaf1c8703d81ee514
nayfaan <luknnn129@gmail.com>
2022-04-27 12:15:49 -0400

    base

--- main.py
+++ main.py
@@ -1,73 +1,41 @@
 import services.startDriver
 from services.startDriver import *
-import csv
-import time
 
 def initiate_driver(driver, url):
     driver.get(url)
     
-def login(driver,username,password):
-    WebDriverWait(driver, timeout=10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "#user_login")))
-    
-    username_field = driver.find_element_by_css_selector("#user_login")
-    username_field.clear()
-    username_field.send_keys(username)
-    
-    WebDriverWait(driver, timeout=10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "#user_pass")))
-    
-    password_field = driver.find_element_by_css_selector("#user_pass")
-    password_field.clear()
-    password_field.send_keys(password)
-    time.sleep(0.3)
-    password_field.send_keys(Keys.RETURN)
-    
-    WebDriverWait(driver, timeout=60).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "#wpcontent")))
-    
-def output_csv(ids):
-    with open('output/output_url.csv', 'w', newline='') as csvfile:
+'''def output_csv(ids):
+    with open('output/output_obj.txt', 'w', newline='') as csvfile:
         output = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for row in ids:
             output.writerow(row)
-            print(row)
+            print(row)'''
 
-def scrap(driver,ids):
+def scrap(driver):
+    items = driver.find_elements_by_css_selector(".elementor-portfolio-item")
+    #.get_attribute("href")
+    urls = []
+    for item in items:
+        urls.append(item.get_attribute("href"))
+        print(urls)
     
-    ids_temp = ids
+    price_list = {}
+    for url in urls:
+        try:
+            driver.get(url)
+            price_list[url] = driver.find_element_by_css_selector(".amount")
+        except Exception as e:
+            print (e)
     
-    try:
-        for row in ids_temp:
-            driver.get(row[0])
-            WebDriverWait(driver, timeout=120).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "#sample-permalink > a")))
-            url_prod = driver.find_element_by_css_selector("#sample-permalink > a").get_attribute("href")
-            row.append(url_prod)
-    except:
-        print("Timeout Error")
-    finally:
-        output_csv(ids_temp)
+    print(price_list)
     
 def run():
-    ids=[]
-    
-    with open('./input/ids.csv', newline='') as csvfile:
-        ids_csv = csv.reader(csvfile, delimiter=',', quotechar='"')
-        
-        for row in ids_csv:
-            url_edit = "https://facedrivesupply.com/wp-admin/post.php?post=" + row[0] + "&action=edit"
-            ids.append(url_edit.split())
-    
     driver = services.startDriver.start()
-    initiate_driver(driver, "https://facedrivesupply.com/wp-admin/edit.php?post_type=product")
-    
-    with open('./input/credentials.txt','r') as f:
-        credentials = f.read().splitlines()
-    
-    
-    login(driver,credentials[0],credentials[1])
-    
+    initiate_driver(driver, "https://facedrivesupply.com/vege")
     try:
-        scrap(driver,ids)
-    except:
-        pass
+        scrap(driver)
+    except Exception as e:
+        print (e)
     finally:
         driver.close()
 
